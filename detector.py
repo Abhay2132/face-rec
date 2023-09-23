07142bbb73924c4fd246b09692247f2d98d22351
Abhay2132 <84978755+Abhay2132@users.noreply.github.com>
2023-09-23 04:44:04 +0000

    i m sleeping

--- detector.py
+++ detector.py
@@ -24,9 +24,35 @@ def encode_known_faces(
         for encoding in face_encodings:
             names.append(name)
             encodings.append(encoding)
-    
+
     name_encodings = {"names": names, "encodings": encodings}
     with encodings_location.open(mode="wb") as f:
         pickle.dump(name_encodings, f)
+
+    
+def recognize_faces(
+    image_location: str,
+    model: str = "hog",
+        encodings_location: Path = DEFAULT_ENCODINGS_PATH,
+) -> None:
+        
+    with encodings_location.open(mode="rb") as f:
+        loaded_encodings = pickle.load(f)
+
+    input_image = face_recognition.load_image_file(image_location)
+
+    input_face_locations = face_recognition.face_locations(
+      input_image, model=model
+    )
+    input_face_encodings = face_recognition.face_encodings(
+        input_image, input_face_locations
+    )
+    for bounding_box, unknown_encoding in zip(
+    input_face_locations, input_face_encodings
+    ):
+        name = _recognize_face(unknown_encoding, loaded_encodings)
+        if not name:
+            name = "Unknown"
+    print(name, bounding_box)
 
 encode_known_faces()
