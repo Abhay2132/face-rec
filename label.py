899e50fd4b3bcc1d79afd353bfa2982f8a161c15
Abhay2132 <84978755+Abhay2132@users.noreply.github.com>
2023-09-23 10:54:27 +0000

    dispaly image

--- label.py
+++ label.py
@@ -2,8 +2,11 @@ from pathlib import Path
 import face_recognition
 import pickle
 from collections import Counter
+from PIL import Image, ImageDraw
 
 DEFAULT_ENCODINGS_PATH = Path("output/encodings.pkl")
+BOUNDING_BOX_COLOR = "blue"
+TEXT_COLOR = "white"
 
 def recognize_faces(
     image_location: str,
@@ -22,13 +25,23 @@ def recognize_faces(
     input_face_encodings = face_recognition.face_encodings(
         input_image, input_face_locations
     )
+
+    # For drawing bounding over image
+    pillow_image = Image.fromarray(input_image)
+    draw = ImageDraw.Draw(pillow_image)
+
+    # Finding Multiple faces in the image
     for bounding_box, unknown_encoding in zip(
     input_face_locations, input_face_encodings
     ):
         name = _recognize_face(unknown_encoding, loaded_encodings)
         if not name:
             name = "Unknown"
-        print(name, bounding_box)
+        # print(name, bounding_box)
+        _display_face(draw, bounding_box, name)
+    
+    del draw
+    pillow_image.show()
 
 def _recognize_face(unknown_encoding, loaded_encodings):
     boolean_matches = face_recognition.compare_faces(
@@ -41,5 +54,23 @@ def _recognize_face(unknown_encoding, loaded_encodings):
     )
     if votes:
         return votes.most_common(1)[0][0]
+
+def _display_face(draw, bounding_box, name):
+    top, right, bottom, left = bounding_box
+    draw.rectangle(((left, top), (right, bottom)), outline=BOUNDING_BOX_COLOR)
+    text_left, text_top, text_right, text_bottom = draw.textbbox(
+        (left, bottom), name
+    )
+    draw.rectangle(
+        ((text_left, text_top), (text_right, text_bottom)),
+        fill="blue",
+        outline="blue",
+    )
+    draw.text(
+        (text_left, text_top),
+        name,
+        fill="white",
+    )
+
 
 recognize_faces("unknown.jpg")
