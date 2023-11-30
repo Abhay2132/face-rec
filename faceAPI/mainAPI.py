d5260a6ae25e202e0833601d13f9879ca74eb4f3
abhay2132 <abhaybro768@gmail.com>
2023-12-01 00:17:37 +0530

    just working

--- faceAPI/mainAPI.py
+++ faceAPI/mainAPI.py
@@ -119,7 +119,7 @@ class FaceAPI:
             cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
             cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
             font = cv2.FONT_HERSHEY_DUPLEX
-            cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+            cv2.putText(frame, name, (left + 6, bottom - 6), font, 0.6, (255, 255, 255), 1)
 
     @staticmethod
     def saveImage(dest , image):
