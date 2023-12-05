e0cd4b7651d50314cf23b6e5378bd524ed1097d5
abhay2132 <abhaybishthestudent@gmail.com>
2023-12-05 19:39:57 +0530

    login ui done

---
+++ camera.py
@@ -0,0 +1,181 @@
+import sys
+import threading
+import time
+import customtkinter as ctk
+import addUser
+import faceAPI.mainAPI
+import ui_
+from PIL import Image
+import cv2
+from pathlib import Path
+from multiprocessing import Array, Process, Manager
+
+sys.path.append("faceAPI")
+from faceAPI import mainAPI
+
+width, height = 800, 600
+
+
+def setData(d):
+    d['kill'] = False
+    d['frame_processed'] = False
+    d['frame'] = []
+    d['data'] = ([],[])
+
+def identifyFaces(_dict):
+    from faceAPI import mainAPI
+    mainAPI.FaceAPI.init()
+
+    while True:
+        time.sleep(0.004)
+        # print("bg:", _dict['frame_processed'], ", data:", _dict['data'])
+        if _dict['kill']:
+            break
+        if len(_dict['frame']) < 1 or (_dict['frame_processed']) :
+            continue
+
+        frame = _dict['frame']
+        persons, locs = mainAPI.FaceAPI.identify(frame, mainAPI.SavedEncoding.encodings)
+        if len(locs) > 0:
+            _dict['data'] = (persons, locs)
+        # print(persons, locs)
+        _dict['frame_processed'] = True
+
+class App(ctk.CTkFrame):
+    cam_h, cam_w = height, width
+    lastTime = 0
+    lastFpsTick = 0
+    data = {"persons": [], "face_locations": []}
+    lastFrame = False
+    kill = False
+    arr = [0, 0]
+    frameRatio = 9/16
+
+    # sharedArr = Array('i')
+    faceAPIProcess = False
+    lastLocations = ([], []) # (persons[], face_locations[])
+
+    def openNewUserDialog(self):
+        self.newUserDialog = addUser.App(self.cap)
+        self.newUserDialog.mainloop()
+
+    def stopThread(self):
+        self.kill = True
+        self.quit()
+
+    def __init__(self, master):
+        super().__init__(master=master)
+        master.bind("<Configure>", self.on_resize)
+        
+        master.protocol("WM_DELETE_WINDOW", self.stopThread)
+        # self.title("Abhay")
+
+        self.mainFrame = ui_.MainFrame(self)
+        self.leftFrame = ui_.LeftFrame(self)
+        self.rightFrame = ui_.RightFrame(self)
+
+        self.grid_columnconfigure((0, 2), minsize=150);
+        self.grid_columnconfigure(1, weight=1, minsize=500);
+        self.grid_rowconfigure(0, minsize=150, weight=1);
+
+        padx = 3
+        pady = 6
+
+        self.mainFrame.grid(padx=padx, pady=pady, row=0, column=1, sticky="nswe")
+        self.leftFrame.grid(row=0, padx=padx, pady=pady, column=0, sticky="nswe")
+        self.rightFrame.grid(row=0, padx=padx, pady=pady, column=2, sticky="nswe")
+
+        self.leftFrame.newUserBtn.configure(command=self.openNewUserDialog)
+
+        self.manager = Manager()
+        self._dict = self.manager.dict()
+
+        setData(self._dict)
+        self.process = Process(target=identifyFaces, args=(self._dict,))
+
+    def initCamera(self):
+        self.cap = cv2.VideoCapture(0)
+        self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, width)
+        self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
+        mainAPI.FaceAPI.init()
+        self.process.start()
+        self.update()
+        return 0
+
+    def updateFPS(self):
+        now = time.time()
+
+        if int(now - self.lastFpsTick) >= 1:
+            dt = (now - self.lastTime)
+            fps = 1 / dt
+            self.mainFrame.fps.configure(text='fps : ' + str(int(fps)))
+            self.lastFpsTick = now
+
+    def update(self):
+        self.updateFPS()
+        self.lastTime = time.time()
+
+        # return 0
+        ret, frame = self.cap.read()
+
+        self.frameRatio = frame.shape[0]/frame.shape[1]
+        if ret:
+            try:
+                frame = cv2.flip(frame, 1)
+                frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+                self._dict['frame'] = frame
+
+                # print(self._dict)
+
+                if(self._dict["frame_processed"]):
+                    print("fg:", self._dict['frame_processed'], "data:", self._dict['data'])
+                    self._dict["frame"] = frame
+                    self.lastLocations = self._dict['data']
+                    self._dict["frame_processed"] = False
+
+                # (persons, face_locations) = mainAPI.FaceAPI.identify(frame, mainAPI.SavedEncoding.encodings)
+                (persons, face_locations) = self.lastLocations
+
+                # print(f"Persons:{persons},locs:{face_locations}")
+                if(len(face_locations) > 0):
+                    mainAPI.FaceAPI.drawBox(frame, face_locations, persons)
+
+                image = Image.fromarray(frame)
+                image_obj = ctk.CTkImage(image, size=(self.cam_w, int(self.cam_h)))
+                self.mainFrame.label.configure(text="", image=image_obj)
+            except Exception as e:
+                print("Error : ", e)
+            finally:
+                self.after(1, self.update)
+        else:
+            self.cap.release()
+            self.mainFrame.label.destroy()
+            self.quit()            
+
+    def on_resize(self, event):
+        w = self.mainFrame.winfo_width()
+        h = self.frameRatio * w
+
+        self.cam_h = h
+        self.cam_w = w
+        self.mainFrame.label.configure(height=h)  # , text=("width :" +str(self.mainFrame.winfo_width())+"px"))
+
+
+if __name__ == "__main__":
+    # ctk.set_default_color_theme("green")
+
+    ctk.set_appearance_mode("light")
+
+    root = ctk.CTk()
+    root.geometry(str(width) + "x" + str(height))
+    root.title("Camera")
+
+    root.grid_rowconfigure(0, weight=1)
+    root.grid_columnconfigure(0, weight=1)
+
+    app = App(root)
+    app.grid(row=0, column=0, sticky="nswe")
+
+    app.after(200, app.initCamera)
+    
+    root.mainloop()
--- login.py
+++ login.py
@@ -1,34 +1,56 @@
 import customtkinter as ctk
+from PIL import ImageTk, Image  
+import os
+
+cwd = os.sep.join(__file__.split(os.sep)[:-1])
+
+class LogoFrame (ctk.CTkFrame):
+    def __init__(self, master):
+        super().__init__(master=master, fg_color="transparent", width=100, height=100)
+
+        logoImage = Image.open(os.path.join(cwd, "assets", "user.png"))
+        # logoImage = ImageTk.PhotoImage(logoImage)
+        logoImage = ctk.CTkImage(logoImage, size=(100,100))
+
+        self.logo = ctk.CTkLabel(master=self, text="", fg_color="transparent", image=logoImage, height=100, width=100)
+        self.logo.grid(row=0, column=0)
 
 class Login(ctk.CTkFrame):
-    def __init__(self, master):
-        super().__init__(master=master, fg_color="#fefefe", corner_radius=10)
+    def __init__(self, master, **kwargs):
+
+        super().__init__(master=master, fg_color="#fefefe")
         
-        self.grid_columnconfigure(0, weight=1)
-        
-        self.logo = ctk.CTkLabel(master=self, text="LOGO")
-        self.logo.grid(row=0, column=0, sticky='we')
-        
-        # self.
-        self.userInput = ctk.CTkEntry(master=self, placeholder_text="Username")
+        self.logoFrame = LogoFrame(self)
+        self.logoFrame.grid(row=0, column=0, padx=10, pady=10)
+
+        self.userInput = ctk.CTkEntry(master=self, placeholder_text="Username", width=300)
         self.userInput.grid(row=1, column=0, sticky='we', padx=50, pady=10)
         
-        self.keyInput = ctk.CTkEntry(master=self, placeholder_text="Password")
-        self.keyInput.grid(row=2, column=0, sticky='we', padx=50, pady=10)
+        self.keyInput = ctk.CTkEntry(master=self, placeholder_text="Password", show="*")
+        self.keyInput.grid(row=2, column=0, sticky='we', padx=50, pady=0)
         
-        self.submit = ctk.CTkButton(master=self, text='Submit')
+        self.submit = ctk.CTkButton(master=self, text='LOGIN', command=self.onSubmit)
         self.submit.grid(row=3, column=0, sticky='ns', padx=10, pady=10)
+
+    def onSubmit(self):
+        user = self.userInput.get()
+        key = self.keyInput.get()
+
+        print(f"user:'{user}',key:'{key}'")
+    
+    def show(self):
+        self.grid(row=0, column=0)
         
 if __name__ == "__main__":
     root = ctk.CTk()
     root.title("LOGIN")
-    root.geometry("600x400")
+    root.geometry("800x600")
     
     root.grid_rowconfigure(0, weight=1)
     root.grid_columnconfigure(0, weight=1)
     
     loginFrame = Login(root)
-    loginFrame.grid(row=0, column=0, sticky="nswe", padx=90, pady=60)
+    loginFrame.show()
     
     # loginFrame.pack()
     
